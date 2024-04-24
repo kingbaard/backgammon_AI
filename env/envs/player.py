52c1d934b84422b3f692d7c185c4eb65d14aa5a2
Mike Leany <55358344+mikeleany@users.noreply.github.com>
2024-04-23 21:14:44 -0600

    Fix issues with bearing off.

--- env/envs/player.py
+++ env/envs/player.py
@@ -1,10 +1,28 @@
+import re
+
+def int_if_possible(val):
+    try:
+        return int(val)
+    except:
+        return val
+
 class Player:
     def __init__(self, id, bar_i, type):
         self.id = id
         self.bar_i = bar_i
+        self.bear_loc = 25 - bar_i
         self.goal = 0
-        self.can_bear = False
         self.type = type
 
     def __str__(self):
         return f"Player {self.id + 1}"
+
+    def get_move(self, game, possible_moves):
+        move = None
+        while not move in possible_moves:
+            input_move = input('Enter move:  ')
+            move = tuple(map(int_if_possible, re.findall(r'[0-9b]+', input_move)))
+            print(move)
+
+        return (move,)
+
